fda55fa47ff4e08934a3987d4095e24fce857fe1
meredithwinchell <meredithwinchell@gmail.com>
2018-06-05 15:12:03 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,2 +1,15 @@
 
 # Code your methods below
+def proper_stance
+  puts "plant legs far apart,bend knees slightly and keep posture loose"
+end 
+
+proper_stance
+
+def base_footwork
+  puts "lift right foot"
+  puts "return right foot to the ground"
+  puts "finishing with a small skip-step backward"
+end
+
+base_footwork
